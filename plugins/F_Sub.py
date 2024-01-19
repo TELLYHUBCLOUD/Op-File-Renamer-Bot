1245f9165a76d5ebd81e1fcd2352e31720e5ee2e
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-01-19 09:41:47 +0530

    Update F_Sub.py

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -5,7 +5,7 @@ from helper.utils import not_subscribed
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
     buttons = [[ InlineKeyboardButton(text="✜ 𝐉𝐨𝐢𝐧 𝐔𝐩𝐃𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ✜", url=client.invitelink) ]]
-    text = "**𝐒𝐨𝐫𝐫𝐲 𝐃𝐮𝐝𝐞 𝐲𝐨𝐮𝐫 𝐍𝐨𝐭 𝐉𝐨𝐢𝐧𝐝 𝐌𝐲 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🦋 𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐓𝐨 𝐔𝐬𝐞 𝐁𝐨𝐭 𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮..**"
+    text = "**𝐒𝐨𝐫𝐫𝐲 𝐃𝐮𝐝𝐞 𝐲𝐨𝐮𝐫 𝐍𝐨𝐭 𝐉𝐨𝐢𝐧𝐝 𝐌𝐲 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐓𝐨 𝐔𝐬𝐞 𝐁𝐨𝐭 𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮..**"
     await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
           
 
